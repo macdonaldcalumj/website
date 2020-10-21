a68464354726557997d205e435d2aaa905d686f7
Calum Macdonald <c@lum.sh>
2020-10-21 19:44:05 +0100

    added education to home page and created references when loading home page source

--- gridsome.server.js
+++ gridsome.server.js
@@ -6,10 +6,30 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = function (api) {
-  api.loadSource(({ addCollection }) => {
+  api.loadSource(({ addCollection, store }) => {
     // Use the Data Store API here: https://gridsome.org/docs/data-store-api/
 
     let homePage = require('./content/index.json')
+    
+    // The JSON file only stores the IDs, actual references need created here.
+    let allEducation = []
+    let allEmployment = []
+
+    if (homePage.education) {
+      for (education of homePage.education) {
+        allEducation.push(store.createReference('Education', education))
+      }
+
+      homePage.education = allEducation
+    }
+
+    if (homePage.employment) {
+      for (employment of homePage.employment) {
+        allEmployment.push(store.createReference('Employment', employment))
+      }
+
+      homePage.employment = allEmployment
+    }
 
     const collection = addCollection({
       typeName: 'HomePage'
