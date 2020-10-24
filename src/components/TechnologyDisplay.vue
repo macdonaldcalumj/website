6d7629d7ea60b3ff96e2385c88555b360f587838
Calum Macdonald <c@lum.sh>
2020-10-24 23:10:13 +0100

    updated project template

---
+++ src/components/TechnologyDisplay.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <g-image :src="technology.image" />
+    {{ technology.name }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    technology: Object,
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  align-items: center;
+  display: flex;
+}
+
+img {
+  height: 0.75em;
+  padding-right: 0.5em;
+}
+</style>
--- src/templates/Project.vue
+++ src/templates/Project.vue
@@ -1,12 +1,26 @@
 <template>
   <Layout>
     <h1>{{ $page.project.title }}</h1>
-    <div>{{ $page.project.date }}</div>
-    <div v-for="technology in $page.project.technologies" :key="technology.id">
-      {{ technology.name }}
-      <g-image :alt="technology.name" :src="technology.image" />
+    <g-image class="main-image" :src="$page.project.image" />
+    <div>Date: {{ $page.project.date }}</div>
+    <div>Technologies:</div>
+    <div
+      class="tech-container"
+      v-for="technology in $page.project.technologies"
+      :key="technology.id"
+    >
+      <TechnologyDisplay :technology="technology" />
     </div>
     <div v-html="$page.project.content" />
+    <div class="grid grid-cols-2">
+      <div
+        class="extra-image-container col-span-2 md:col-span-1"
+        v-for="image in $page.project.extraImages"
+        :key="image.id"
+      >
+        <g-image class="extra-image" :src="image" />
+      </div>
+    </div>
   </Layout>
 </template>
 
@@ -14,6 +28,7 @@
 query($id: ID!) {
   project: project(id: $id) {
     title
+    image
     date
     technologies {
       id
@@ -21,12 +36,18 @@ query($id: ID!) {
       image
     }
     content
+    extraImages
   }
 }
 </page-query>
 
 <script>
+import TechnologyDisplay from '@/components/TechnologyDisplay.vue';
+
 export default {
+  components: {
+    TechnologyDisplay,
+  },
   metaInfo() {
     return {
       title: this.$page.project.title,
@@ -35,4 +56,28 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+h1 {
+  font-size: 2rem;
+  font-weight: bold;
+  line-height: 4.5rem;
+}
+
+.extra-image {
+  margin: auto;
+  width: 80%;
+}
+
+.extra-image-container {
+  margin: 1.5rem 0;
+}
+
+.main-image {
+  height: 15rem;
+  margin-bottom: 1.5rem;
+}
+
+.tech-container {
+  margin-left: 5rem;
+}
+</style>
