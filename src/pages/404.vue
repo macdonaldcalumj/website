393a8b50d4533a28fbdf8f6257cf883034480ecb
Calum Macdonald <c@lum.sh>
2020-10-28 17:57:35 +0000

    added 404 page

---
+++ src/pages/404.vue
@@ -0,0 +1,14 @@
+<template>
+  <Layout>
+    <div>
+      <h2>404</h2>
+      <div>Sorry, that page was not found!</div>
+    </div>
+  </Layout>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
